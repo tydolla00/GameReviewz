7bd65582e7293902669d7a9f59945f7e8249a4f9
John Beltran <johnbeltran4561@gmail.com>
2021-12-14 07:54:36 -0500

    Changed files

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,91 @@
-let x = 7
-let y
-if(x === "seven") {
-y = "x is seven"
-} else {
-y = "x is something else"
+class StarRating extends HTMLElement {
+    get value () {
+        return this.getAttribute('value') || 0;
+    }
+
+    set value (val) {
+        this.setAttribute('value', val);
+        this.highlight(this.value - 1);
+    }
+
+    get number () {
+        return this.getAttribute('number') || 5;
+    }
+
+    set number (val) {
+        this.setAttribute('number', val);
+
+        this.stars = [];
+
+        while (this.firstChild) {
+            this.removeChild(this.firstChild);
+        }
+
+        for (let i = 0; i < this.number; i++) {
+            let s = document.createElement('div');
+            s.className = 'star';
+            this.appendChild(s);
+            this.stars.push(s);
+        }
+
+        this.value = this.value;
+    }
+
+    highlight (index) {
+        this.stars.forEach((star, i) => {
+            star.classList.toggle('full', i <= index);
+        });
+    }
+
+    constructor () {
+        super();
+
+        this.number = this.number;
+
+        this.addEventListener('mousemove', e => {
+            let box = this.getBoundingClientRect(),
+                starIndex = Math.floor((e.pageX - box.left) / box.width * this.stars.length);
+
+            this.highlight(starIndex);
+        });
+
+        this.addEventListener('mouseout', () => {
+            this.value = this.value;
+        });
+
+        this.addEventListener('click', e => {
+            let box = this.getBoundingClientRect(),
+                starIndex = Math.floor((e.pageX - box.left) / box.width * this.stars.length);
+
+            this.value = starIndex + 1;
+
+            let rateEvent = new Event('rate');
+            this.dispatchEvent(rateEvent);
+        });
+    }
+}
+
+customElements.define('x-star-rating', StarRating);
+
+let darkBut = document.querySelector(".darkMode")
+darkBut.addEventListener("click",darkMode)
+pressed = false; 
+
+function darkMode() {
+    //change background color to dark and text color to something light
+    pressed = !pressed
+    light = "Light Mode"
+    dark = "Dark Mode"
+    if(pressed){
+    document.body.style.backgroundColor = "#1e272c";
+    document.body.style.color = "white";
+    document.querySelector(".darkMode").innerText = light;
+    document.getElementById("logo").src="img/logodark.png"
+    }
+    else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        document.querySelector(".darkMode").innerText = dark;
+        document.getElementById("logo").src="img/logowhite.png"
+    }
 }
